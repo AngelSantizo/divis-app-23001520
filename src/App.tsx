8f111bcc28473f6418c28ab6a3bf8863cb56b48e
Angel Santizo <aangelsanti12@gmail.com>
2025-11-22 16:02:39 -0600

    Mejora en el frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,105 @@
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { IonApp, IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonItem, IonInput, IonLabel, IonText, IonFooter, setupIonicReact } from '@ionic/react';
+import { useState } from 'react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [quetzales, setQuetzales] = useState<number>(0);
+  const exchangeRate = 8; // tomaremos como ejemplo que por cada 8 quetzales 1 dolar
+  
+  const handleInputChange = (e: CustomEvent) => {
+    const value = parseFloat((e.target as HTMLInputElement).value) || 0;
+    setQuetzales(value);
+  };
+
+  const usdAmount = (quetzales/exchangeRate).toFixed(2);
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar color="primary" className="ion-text-center">
+          <IonTitle>DivisApp 23001520</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      
+      <IonContent className="ion-padding">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          maxWidth: '600px',
+          margin: '0 auto',
+          padding: '20px'
+        }}>
+          <IonGrid>
+            <IonRow className="ion-justify-content-center">
+              <IonCol size="12" sizeMd="8" sizeLg="6">
+                <IonItem>
+                  <IonInput 
+                    type="number"
+                    label="Cantidad en quetzales"
+                    labelPlacement="floating"
+                    onIonInput={handleInputChange}
+                    value={quetzales}
+                    className="ion-text-center"
+                    inputmode="decimal"
+                    style={{
+                      '--padding-start': '10px',
+                      '--padding-end': '10px',
+                      'width': '100%',
+                      'margin': '10px 0'
+                    }}
+                  >
+                    <IonText slot="end" color="medium" style={{ marginLeft: '8px' }}>GTQ</IonText>
+                  </IonInput>
+                </IonItem>
+                
+                <IonItem lines="none" style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  textAlign: 'center',
+                  marginTop: '20px',
+                  '--background': 'transparent'
+                }}>
+                  <IonLabel>
+                    <h2>Equivalente en dólares:</h2>
+                    <div style={{
+                      fontSize: '2rem',
+                      fontWeight: 'bold',
+                      color: 'var(--ion-color-primary)',
+                      margin: '10px 0'
+                    }}>
+                      ${usdAmount} USD
+                    </div>
+                    <IonText color="medium">Tipo de cambio: 1 USD = Q{exchangeRate} GTQ</IonText>
+                  </IonLabel>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+      </IonContent>
+
+      <IonFooter>
+        <IonToolbar style={{textAlign: 'center', fontWeight: 'bold'}}>
+          <IonTitle color="primary">Angel Santizo 23001520</IonTitle>
+        </IonToolbar>
+      </IonFooter>
+    </IonApp>
+  );
+}
 
 export default App;
